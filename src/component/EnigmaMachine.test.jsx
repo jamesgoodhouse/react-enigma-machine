0e0d3eb8710147c991596c6fc971e16b89fb7ae9
James J. Goodhouse <j.goodhouse@gmail.com>
2022-04-19 19:25:53 -0700

    remove unused test

--- src/component/EnigmaMachine.test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import EnigmaMachine from './EnigmaMachine';
-
-test('renders learn react link', () => {
-  render(<EnigmaMachine />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
